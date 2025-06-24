8e31a276de80bbd5aaef2518aed98a68e8ab572b
KiranDIgari <kirandigari6@gmail.com>
2025-06-24 22:06:21 +0530

    end commit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,9 +13,12 @@ wget https://github.com/adoptium/temurin19-binaries/releases/download/jdk-19.0.2
 mkdir -p /opt/java
 tar -xvzf OpenJDK19U-jdk_x64_linux_hotspot_19.0.2_7.tar.gz -C /opt/java
 
+# Set Java environment variables for this script AND system-wide
+export JAVA_HOME=/opt/java/jdk-19.0.2+7
+export PATH=$JAVA_HOME/bin:$PATH
+
 echo "export JAVA_HOME=/opt/java/jdk-19.0.2+7" >> /etc/profile
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile
-source /etc/profile
 
 # Verify Java
 java -version
