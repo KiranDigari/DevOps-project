31ec6bb9de3c796494872833a440c18d7fbc31a3
KiranDIgari <kirandigari6@gmail.com>
2025-06-24 21:46:52 +0530

    last commit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Log all output to a file
+exec > /var/log/setup-script.log 2>&1
+set -e
+
 # Install dependencies
 apt update -y
 apt install -y git curl wget maven
@@ -8,8 +12,10 @@ apt install -y git curl wget maven
 wget https://github.com/adoptium/temurin19-binaries/releases/download/jdk-19.0.2+7/OpenJDK19U-jdk_x64_linux_hotspot_19.0.2_7.tar.gz
 mkdir -p /opt/java
 tar -xvzf OpenJDK19U-jdk_x64_linux_hotspot_19.0.2_7.tar.gz -C /opt/java
-export JAVA_HOME=/opt/java/jdk-19.0.2+7
-export PATH=$JAVA_HOME/bin:$PATH
+
+echo "export JAVA_HOME=/opt/java/jdk-19.0.2+7" >> /etc/profile
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile
+source /etc/profile
 
 # Verify Java
 java -version
@@ -17,8 +23,8 @@ java -version
 # Clone and run the app
 cd /opt
 git clone https://github.com/KiranDigari/DevOps-project.git
-cd DevOps-project/assignment-1
-mvn spring-boot:run &
+cd DevOps-project
+mvn spring-boot:run > /opt/app.log 2>&1 &
 
-# Auto shutdown (optional)
+# Auto shutdown
 shutdown -h +10
