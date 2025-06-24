9616df33237330ccc8e505b1c3c882aeac7394ed
KiranDIgari <kirandigari6@gmail.com>
2025-06-24 21:09:44 +0530

    final commit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
 apt update -y
-apt install -y git curl wget
+apt install -y git sudo curl wget
 
-
+# Install Java 19
 wget https://github.com/adoptium/temurin19-binaries/releases/download/jdk-19.0.2+7/OpenJDK19U-jdk_x64_linux_hotspot_19.0.2_7.tar.gz
 mkdir -p /opt/java
 tar -xvzf OpenJDK19U-jdk_x64_linux_hotspot_19.0.2_7.tar.gz -C /opt/java
 export JAVA_HOME=/opt/java/jdk-19.0.2+7
-export PATH=$JAVA_HOME/bin:$PATHgit commit -m "Update setup.sh with final Java setup"
+export PATH=$JAVA_HOME/bin:$PATH
 
-# Verify Java installation
+# Verify Java
 java -version
 
-# Clone your GitHub repo
+# Clone repo
 cd /opt
 git clone https://github.com/KiranDigari/DevOps-project.git
-cd DevOps-project/assignment-1
-
-# Placeholder for future .jar
-# sudo nohup java -jar your-app.jar --server.port=80 &
+cd DevOps-project/assignment-1 || exit 1
 
 # Auto-shutdown after X minutes
-sudo shutdown -h +$${shutdown_after_minutes}
-
+sudo shutdown -h +${shutdown_after_minutes}
