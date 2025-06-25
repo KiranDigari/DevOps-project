573bc72bc925d2e715a9ea2e12f26a9b61567083
KiranDIgari <kirandigari6@gmail.com>
2025-06-25 16:03:54 +0530

    ec2 update

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -35,3 +35,22 @@ resource "aws_security_group" "web_sg" {
     Stage = var.stage
   }
 }
+resource "aws_instance" "app" {
+  ami                         = "ami-0f918f7e67a3323f0"  # Make sure this AMI is valid for ap-south-1
+  instance_type               = var.instance_type
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  key_name                    = "project.key"  # Ensure this key exists in your AWS region
+  iam_instance_profile        = aws_iam_instance_profile.write_profile.name
+
+  user_data = templatefile("${path.module}/../scripts/setup.sh", {
+    repo_url               = var.repo_url,
+    shutdown_after_minutes = var.shutdown_after_minutes,
+    bucket_name            = var.bucket_name
+  })
+
+  tags = {
+    Name  = "App-${var.stage}"
+    Stage = var.stage
+  }
+}
+
