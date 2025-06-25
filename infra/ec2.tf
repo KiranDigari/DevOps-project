1034b23f69f455c6b713aecf91cf77c3e556f709
KiranDIgari <kirandigari6@gmail.com>
2025-06-25 15:12:15 +0530

    update

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,18 +1,37 @@
-resource "aws_instance" "app" {
-  ami                    = "ami-0f918f7e67a3323f0"  
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name               = "project.key"
-  iam_instance_profile   = aws_iam_instance_profile.write_profile.name
+data "aws_vpc" "default" {
+  default = true
+}
 
-  user_data = templatefile("${path.module}/../scripts/setup.sh", {
-    repo_url               = var.repo_url,
-    shutdown_after_minutes = var.shutdown_after_minutes,
-    bucket_name            = var.bucket_name
-  })
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name  = "App-${var.stage}"
+    Name  = "web-sg"
     Stage = var.stage
   }
 }
